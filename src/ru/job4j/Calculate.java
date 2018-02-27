2961beb2074ea68674abce185d6f1b523c21a1e3
sars33 <sergetester33@gmail.com>
2018-02-27 13:10:07 -0800

    1.1. Создание Hello World[#141]

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+/**
+ * Calculate.
+ *
+ * @author Sergey Arsenyev (sarsenyev@hotmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Calculate {
+	/**
+     * Main.
+     * @param String - args.
+     */
+	public static void main(String[] args) {
+	
+	System.out.println("Hello world");
+	}
+}
