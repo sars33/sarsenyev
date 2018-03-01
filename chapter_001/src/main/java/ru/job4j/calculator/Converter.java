adc32846d451418cd2e72b6ccb48e1b99e3abbdf
sars33 <sergetester33@gmail.com>
2018-02-28 21:14:34 -0800

    3.2. Конвертер валюты. [41583]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -0,0 +1,52 @@
+package ru.job4j.calculator;
+
+
+    /**
+     * Конвертор валюты.
+     */
+    public class Converter {
+
+       int  rubelCurrencyToEuro = 70;
+       int rubelCurrencyToDollar = 60;
+
+
+
+
+        /**
+         * Конвертируем рубли в евро.
+         * @param value рубли.
+         * @return Евро.
+         */
+        public int rubleToEuro(int value) {
+            return value/rubelCurrencyToEuro;
+
+        }
+
+        /**
+         * Конвертируем рубли в доллары.
+         * @param value рубли.
+         * @return Доллары
+         */
+        public int rubleToDollar(int value) {
+            return value/rubelCurrencyToDollar;
+
+        }
+        /**
+         * Конвертируем евро в рубли.
+         * @param value евро.
+         * @return рубли
+         */
+        public int euroToRubel(int value) {
+            return value*rubelCurrencyToEuro;
+
+    }
+        /**
+         * Конвертируем дсллары в рубли.
+         * @param value доллары.
+         * @return рубли
+         */
+        public int DollarToRubel(int value) {
+            return value*rubelCurrencyToDollar;
+        }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConverterTest {
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        double result = converter.rubleToDollar(60);
+        assertThat(result, is(1D));
+    }
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        double result = converter.rubleToEuro(70);
+        assertThat(result, is(1D));
+    }
+    @Test
+    public void whenOneEuroToRubelsThen60() {
+        Converter converter = new Converter();
+        double result = converter.euroToRubel(1);
+        assertThat(result, is(70D));
+    }
+    @Test
+    public void whenOneDollarToRubelsThen60() {
+        Converter converter = new Converter();
+        double result = converter.DollarToRubel(1);
+        assertThat(result, is(60D));
+    }
+
+}
