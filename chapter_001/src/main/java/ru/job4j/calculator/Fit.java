1f4f731ba171a04d4428719e979db1939aee5c97
sars33 <sergetester33@gmail.com>
2018-03-01 23:29:37 -0800

    3.3. Идеальный вес. [#41584]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+/**
+ * Ideal weight calculator
+ */
+
+public class Fit {
+
+
+    /**
+     * Ideal weight for a man.
+     * @param height Height.
+     * @return ideal weight.
+     */
+        double manWeight(double height) {
+        return (height - 100) * 1.15;
+
+    }
+    /**
+     * Ideal weight for a woman
+     * @param height Height
+     * @return ideal weight
+     */
+    double womanWeight(double height){
+        return (height - 110) * 1.15;
+    }
+
+}
