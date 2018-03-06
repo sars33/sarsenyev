0f4ce1bf39042acfc9b7da5e12e6366335709bf4
sars33 <sergetester33@gmail.com>
2018-03-05 20:01:58 -0800

    3.2. Конвертер валюты. [41583]

--- chapter_001/src/main/java/ru/job4j/calculator/Converter.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -1,53 +1,56 @@
 package ru.job4j.calculator;
 
 
+/**
+ * Конвертор валюты.
+ */
+public class Converter {
+
+    private static final int EUROCURRENT = 70;
+    private static final int DOLLARCURRENT = 60;
+
+
+
+
     /**
-     * Конвертор валюты.
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
      */
-    public class Converter {
-
-       int  rubelCurrencyToEuro = 70;
-       int rubelCurrencyToDollar = 60;
-
-
-
-
-        /**
-         * Конвертируем рубли в евро.
-         * @param value рубли.
-         * @return Евро.
-         */
-        public int rubleToEuro(int value) {
-            return value/rubelCurrencyToEuro;
-
-        }
-
-        /**
-         * Конвертируем рубли в доллары.
-         * @param value рубли.
-         * @return Доллары
-         */
-        public int rubleToDollar(int value) {
-            return value/rubelCurrencyToDollar;
-
-        }
-        /**
-         * Конвертируем евро в рубли.
-         * @param value евро.
-         * @return рубли
-         */
-        public int euroToRubel(int value) {
-            return value*rubelCurrencyToEuro;
+    public int rubleToEuro(int value) {
+        int euro = value/EUROCURRENT;
+        return euro;
 
     }
-        /**
-         * Конвертируем дсллары в рубли.
-         * @param value доллары.
-         * @return рубли
-         */
-        public int DollarToRubel(int value)
-        {
-            return value*rubelCurrencyToDollar;
-        }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллары
+     */
+    public int rubleToDollar(int value) {
+        int dollar = value/DOLLARCURRENT;
+        return dollar;
+
+    }
+    /**
+     * Конвертируем евро в рубли.
+     * @param value евро.
+     * @return рубли
+     */
+    public int euroToRubel(int value) {
+        int rubel = value*EUROCURRENT;
+        return rubel;
+
+    }
+    /**
+     * Конвертируем дсллары в рубли.
+     * @param value доллары.
+     * @return рубли
+     */
+    public int DollarToRubel(int value) {
+        int rubel = value*DOLLARCURRENT;
+        return rubel;
+    }
 
 }
